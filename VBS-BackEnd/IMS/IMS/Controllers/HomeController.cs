8b26808f5601367001e0b704e5810705c437bfbb
Domien De Pauw <147160765+DomienDePauw@users.noreply.github.com>
2024-05-25 20:13:09 +0200

    OK

--- VBS-BackEnd/IMS/IMS/Controllers/HomeController.cs
+++ VBS-BackEnd/IMS/IMS/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
 
 namespace InventoryManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        string cs = ConfigurationManager.ConnectionStrings["ImsConnectionstring"].ConnectionString;
+
         public ActionResult Index()
         {
             return View();
