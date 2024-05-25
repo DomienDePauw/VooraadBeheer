4650fabd1b645123cdcac7ec7b15ecf0fc939770
Domien De Pauw <147160765+DomienDePauw@users.noreply.github.com>
2024-05-25 19:01:02 +0200

    New

--- VBS-BackEnd/VBS-BackEnd/Libraries/Infrastructure/SubGroups.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VBS_BackEnd.Models {
-
-    public class SubGroups {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int GroupId { get; set; }
-    }
-}
