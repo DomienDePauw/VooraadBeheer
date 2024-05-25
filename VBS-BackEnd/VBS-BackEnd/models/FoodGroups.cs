8dd22c9da486f08ab5e04d383e277e720f75ab33
JosimCoussens <coussensjosim@yahoo.com>
2024-05-25 16:50:49 +0200

    added .sql file

---
+++ VBS-BackEnd/VBS-BackEnd/models/FoodGroups.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VBS_BackEnd.Models;
+
+public class FoodGroups {
+    public int FoodGroupId { get; set; }
+}
