a0e229718986b8912be0cf8ab952529c5615d6f5
Domien De Pauw <147160765+DomienDePauw@users.noreply.github.com>
2024-05-25 16:24:33 +0200

    Models
    Dish en Foodinventory

--- VBS-BackEnd/VBS-BackEnd/models/Dish.cs
+++ VBS-BackEnd/VBS-BackEnd/models/Dish.cs
@@ -7,5 +7,25 @@ namespace VBS_BackEnd.Models;
 
 public class Dish
 {
+    public int FoodId { get; set; }
+    public string Name { get; set; }
+    public string PhotoUrl { get; set; }
+    public int FoodGroupId { get; set; }
+    public int SubGroupId { get; set; }
+    public int Quantity { get; set; }
+    public DateTime StockDate { get; set; }
+    public DateTime ExpiryDate { get; set; }
+
+    public Dish(int foodId, string name, string photoUrl, int foodGroupId, int subGroupId, int quantity, DateTime stockDate, DateTime expiryDate)
+    {
+        FoodId = foodId;
+        Name = name;
+        PhotoUrl = photoUrl;
+        FoodGroupId = foodGroupId;
+        SubGroupId = subGroupId;
+        Quantity = quantity;
+        StockDate = stockDate;
+        ExpiryDate = expiryDate;
+    }
 
 }
