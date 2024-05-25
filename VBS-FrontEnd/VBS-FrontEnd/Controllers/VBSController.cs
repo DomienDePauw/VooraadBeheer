3d3b64f8c27ac0e7ec03143a5504e56877a0ec10
Dean Schelfhout <schelfhout.dean@gmail.com>
2024-05-25 13:54:19 +0200

    Added new pages and updated controller
    Nieuwe pages gemaakt en de action results aangemaakt in de nu VBS controller

---
+++ VBS-FrontEnd/VBS-FrontEnd/Controllers/VBSController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VBS_FrontEnd.Controllers
+{
+    public class VBSController : Controller
+    {
+        public ActionResult Home()
+        {
+            List<string> list = new List<string>();
+            list.Add("u mama");
+            list.Add("u pzpz");
+            list.Add("u qzdqzd");
+            list.Add("u gggg");
+            list.Add("u rikkie");
+            return View(list);
+        }
+
+        public ActionResult AddFood()
+        {
+            ViewBag.Message = "Add your food here.";
+
+            return View();
+        }
+
+        public ActionResult InventoryManagement()
+        {
+            ViewBag.Message = "General Management page page.";
+
+            return View();
+        }
+        public ActionResult Recipe()
+        {
+            ViewBag.Message = "Specific recipe.";
+
+            return View();
+        }
+        public ActionResult Recipes()
+        {
+            ViewBag.Message = "Alle recipes.";
+
+            return View();
+        }
+        public ActionResult Stock()
+        {
+            ViewBag.Message = "Stock overview.";
+
+            return View();
+        }
+    }
+}
