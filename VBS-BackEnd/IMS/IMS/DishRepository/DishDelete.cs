fc6882f70f014a3a26635aac50cf401b8223bcf5
Domien De Pauw <147160765+DomienDePauw@users.noreply.github.com>
2024-05-25 20:21:41 +0200

    CRUD

---
+++ VBS-BackEnd/IMS/IMS/DishRepository/DishDelete.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.Configuration;
+
+namespace InventoryManagementSystem.DishRepository
+{
+    public class DishDelete
+    {
+        private string cs = WebConfigurationManager.ConnectionStrings["ImsConnectionstring"].ConnectionString;
+
+        public bool DeleteDish(int id)
+        {
+            bool success = false;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    string sqlQuery = "DELETE FROM Dishes WHERE Id = @Id";
+
+                    using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", id);
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            success = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log exception details if needed
+                Console.WriteLine(ex.ToString());
+            }
+
+            return success;
+        }
+    }
+}
