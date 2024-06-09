0eb77f48ad025ce8963a25603f72d7585fca8ead
JosimCoussens <coussensjosim@yahoo.com>
2024-06-09 14:55:38 +0200

    Finished method  GetDishesByGroupId

--- VBS-Api/Controllers/VBSController.cs
+++ VBS-Api/Controllers/VBSController.cs
@@ -7,32 +7,28 @@ using System.Data.SqlClient;
 using System.Text.Json.Serialization;
 using VBS_Api.Models;
 
-namespace VBS_Api.Controllers
-{
+namespace VBS_Api.Controllers {
+
     [Route("api/[controller]")]
     [ApiController]
-    public class VBSController : ControllerBase
-    {
+    public class VBSController : ControllerBase {
         public readonly IConfiguration _configuration;
-        public VBSController(IConfiguration configuration)
-        {
+
+        public VBSController(IConfiguration configuration) {
             _configuration = configuration;
         }
 
         [HttpGet]
         [Route("GetAllDishes")]
-        public List<Dish> GetDishes()
-        {
+        public List<Dish> GetDishes() {
             SqlConnection con = new SqlConnection(_configuration.GetConnectionString("VBSDbConnectionString").ToString());
             SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Dish", con);
             DataTable dt = new DataTable();
             da.Fill(dt);
 
-            List<Dish>dishes = new List<Dish>();
-            if (dt.Rows.Count > 0)
-            {
-                foreach (DataRow dataRow in dt.Rows)
-                {
+            List<Dish> dishes = new List<Dish>();
+            if (dt.Rows.Count > 0) {
+                foreach (DataRow dataRow in dt.Rows) {
                     string req = (string)dataRow["Requirements"];
                     List<string> reqs = new List<string>();
                     reqs = req.Trim().Split(',').ToList();
@@ -41,8 +37,7 @@ namespace VBS_Api.Controllers
                     string quantitiy = (string)dataRow["QuantityPer100GramAmount"];
                     quantities = quantitiy.Trim().Split(",").ToList().Select(int.Parse).ToList();
 
-                    Dish dish = new Dish()
-                    {
+                    Dish dish = new Dish() {
                         Id = (int)dataRow["Id"],
                         Name = (string)dataRow["Name"],
                         PhotoUrl = (string)dataRow["PhotoUrl"],
@@ -58,20 +53,49 @@ namespace VBS_Api.Controllers
             }
             return dishes;
         }
+
         [HttpGet]
         [Route("GetAllDishesByGroupId{Id}")]
-        public List<Dish> GetDishesByGroupId(int Id)
-        {
+        public List<DishWithNames> GetDishesByGroupId(int Id) {
             SqlConnection con = new SqlConnection(_configuration.GetConnectionString("VBSDbConnectionString").ToString());
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Dish", con);
+            string q1 = "SELECT GroupId, SubgroupId FROM Dish WHERE Id = @Id ";
+            SqlCommand cmd = new SqlCommand(q1, con);
+            cmd.Parameters.AddWithValue("@Id", Id);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
 
             //je krijgt van de front End een Id van een Dish.
             //De front End wilt een lijst met alle dishes die dezelfde groupId en subgroupId hebben (inclusief de dish van het gegeven Id)
             //Er zal een nieuwe model moeten gemaakt worden van Dish, zodat de front End niet groupId en subgroupId heeft, maar een group name en subgroup name
-
-
+            dynamic dishDetails = new { };
+            foreach (DataRow dataRow in dt.Rows) {
+                dishDetails = new {
+                    GroupId = (int)dataRow["GroupId"],
+                    SubgroupId = (int)dataRow["SubgroupId"]
+                };
+            }
+            string q2 = "SELECT d.Id, d.Name, d.PhotoUrl, d.Requirements, d.QuantityPer100GramAmount, d.UnitInStock, g.Name AS GroupName, sg.Name AS SubgroupName FROM Dish d JOIN Groups g ON d.GroupId=g.Id JOIN Subgroups sg ON d.SubgroupId = sg.Id WHERE d.GroupId = @GroupId AND SubgroupId = @SubgroupId";
+            SqlCommand cmd2 = new SqlCommand(q2, con);
+            cmd2.Parameters.AddWithValue("@groupId", dishDetails.GroupId);
+            cmd2.Parameters.AddWithValue("@subgroupId", dishDetails.SubgroupId);
+            SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            List<DishWithNames> dishes = new List<DishWithNames>();
+            foreach (DataRow dataRow in dt2.Rows) {
+                DishWithNames newDish = new DishWithNames() {
+                    Id = (int)dataRow["Id"],
+                    Name = (string)dataRow["Name"],
+                    PhotoUrl = (string)dataRow["PhotoUrl"],
+                    Requirements = dataRow["Requirements"].ToString().Trim().Split(", ").ToList(),
+                    QuantityPer100GramAmount = dataRow["QuantityPer100GramAmount"].ToString().Trim().Split(", ").ToList().Select(int.Parse).ToList(),
+                    UnitInStock = (string)dataRow["UnitInStock"],
+                    GroupName = (string)dataRow["GroupName"],
+                    SubgroupName = (string)dataRow["SubgroupName"]
+                };
+                dishes.Add(newDish);
+            }
             return dishes;
         }
     }
--- VBS-Api/Models/DishWithNames.cs
+++ VBS-Api/Models/DishWithNames.cs
@@ -1,13 +1,12 @@
 ﻿namespace VBS_Api.Models {
-
-    public class Dish {
+    public class DishWithNames {
         public int Id { get; set; }
         public string Name { get; set; }
         public string PhotoUrl { get; set; }
         public List<string> Requirements { get; set; }
         public List<int> QuantityPer100GramAmount { get; set; }
         public string UnitInStock { get; set; }
-        public int GroupId { get; set; }
-        public int SubgroupId { get; set; }
+        public string GroupName { get; set; }
+        public string SubgroupName { get; set; }
     }
 }
